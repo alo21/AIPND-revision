4544f952de6d424660c2f0e6f187e45588ed026a
alessandrolosavio <alessandro.losavio@mail.polimi.it>
2019-10-26 14:37:24 +0200

    - add command line args

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -22,22 +22,11 @@ import argparse
 #       collection that you created with this function
 # 
 def get_input_args():
-    """
-    Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
-    Command Line Arguments:
-      1. Image Folder as --dir with default value 'pet_images'
-      2. CNN Model Architecture as --arch with default value 'vgg'
-      3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
-    This function returns these arguments as an ArgumentParser object.
-    Parameters:
-     None - simply using argparse module to create & store command line arguments
-    Returns:
-     parse_args() -data structure that stores the command line arguments object  
-    """
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+
+    parser = argparse.ArgumentParser(description='Use a Pre-trained Image Classifier to Identify Dog Breeds')
+
+    parser.add_argument("--dir", help='Specify images directory')
+    parser.add_argument("--arch", help='Specify the model you want to use')
+    parser.add_argument("--dogfile", help='Specify the file that contains dognames')
+
+    return parser.parse_args()
