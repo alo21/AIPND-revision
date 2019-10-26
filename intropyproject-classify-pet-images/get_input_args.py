c686db72a52526a1a0737863d140a02e16491b12
alessandrolosavio <alessandro.losavio@mail.polimi.it>
2019-10-26 17:58:35 +0200

    - create dictionary with file and filename

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: Alessandro Losavio
+# DATE CREATED: 26/10/2019
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Alessandro Losavio
+# DATE CREATED: 26/10/2019
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -19,27 +19,21 @@
 # Imports python modules
 from os import listdir
 
+
 # TODO 2: Define get_pet_labels function below please be certain to replace None
 #       in the return statement with results_dic dictionary that you create 
 #       with this function
 # 
 def get_pet_labels(image_dir):
-    """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
-    filenames of the images contain the true identity of the pet in the image.
-    Be sure to format the pet labels so that they are in all lower case letters
-    and with leading and trailing whitespace characters stripped from them.
-    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
-    Parameters:
-     image_dir - The (full) path to the folder of images that are to be
-                 classified by the classifier function (string)
-    Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
-      List. The list contains for following item:
-         index 0 = pet image label (string)
-    """
+    files = listdir(image_dir)
+    results_dic = {}
+
+    for file in files:
+        formattedString = file.replace(".jpg", "").replace("_", " ").lower()
+        stringWithNoDigits = ''.join([i for i in formattedString if not i.isdigit()]).strip()
+
+        results_dic.update({file: [stringWithNoDigits]})
+
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    return results_dic
